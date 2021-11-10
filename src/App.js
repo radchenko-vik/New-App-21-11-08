e91a7742011db3f9de665fdf46a2c96fd7aebafa
Vitalii <radchenko.rapa375@gmail.com>
2021-11-10 12:32:36 +0200

    Add html structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const App = () =>
-    <div className="App">
-hello
+const App = () => (
+    <div className='App'>
+        <div className='container'>
+            <div className='todo-app'>
+                <header>
+                    <h1>todo app</h1>
+                </header>
+                <div className='todo-creator'>
+                    <input type='text' className='message' />
+                    <button className='btn add-todo'>add</button>
+                </div>
+                <div className='todos-list'>
+                    <ul className='todos'>
+                        <li className='todo-item'>some todo`s</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
+);
 
 export default App;
